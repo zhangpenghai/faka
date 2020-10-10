7b5ca321c857db97ca5c6d2153760d36d8a83d42
rea <1039902405@qq.com>
2020-10-10 22:18:42 +0800

    粗版

---
+++ src/main/java/com/aagw/faka/FakaApplication.java
@@ -0,0 +1,15 @@
+package com.aagw.faka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class FakaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aagw/faka/FakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aagw.faka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
